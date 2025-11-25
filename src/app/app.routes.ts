351037e8fe8f32951cb5f71f916449f9c21f9b82
Daft4Less <jose.jsu@gmail.com>
2025-11-25 18:59:36 -0500

    feat: Creacion de la vista Inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import path from 'path';
+import { Inicio } from './pages/inicio/inicio';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Inicio},
+    {path: 'inicio', component: Inicio},
+    {path: '**', redirectTo: ''}
+];
