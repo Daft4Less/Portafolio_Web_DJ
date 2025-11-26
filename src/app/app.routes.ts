18401713a0d7db2796a49009e143d1fde7d0a464
Daft4Less <jose.jsu@gmail.com>
2025-11-25 20:44:04 -0500

    feat: botones y rutas funcionales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import path from 'path';
 import { Inicio } from './publico/inicio/inicio';
 import { Programadores } from './publico/programadores/programadores';
 import { VerPerfil } from './publico/ver-perfil/ver-perfil';
+import { Agendar } from './publico/agendar/agendar';
 
 export const routes: Routes = [
     {path: '', component: Inicio},
     {path: 'inicio', component: Inicio},
     {path: 'programadores', component: Programadores},
     {path: 'empleado', component: VerPerfil},
+    {path: 'agendar', component: Agendar},
     {path: '**', redirectTo: ''}
 ];
--- src/app/publico/ver-perfil/ver-perfil.ts
+++ src/app/publico/ver-perfil/ver-perfil.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-ver-perfil',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './ver-perfil.html',
   styleUrl: './ver-perfil.scss',
 })
