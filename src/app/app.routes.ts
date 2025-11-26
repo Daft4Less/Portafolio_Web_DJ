744aa43f65091ccbb1407e7bdbcf4b3d3f9949df
Daft4Less <jose.jsu@gmail.com>
2025-11-25 20:19:34 -0500

    feat: ver perfil de los programadores

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import path from 'path';
 import { Inicio } from './publico/inicio/inicio';
+import { Programadores } from './publico/programadores/programadores';
+import { VerPerfil } from './publico/ver-perfil/ver-perfil';
 
 export const routes: Routes = [
     {path: '', component: Inicio},
     {path: 'inicio', component: Inicio},
+    {path: 'programadores', component: Programadores},
+    {path: 'empleado', component: VerPerfil},
     {path: '**', redirectTo: ''}
 ];
