d6ad37e0fff1ab7cf6f641d086696901b49e2c76
dani5G <dauquilla50@gmail.com>
2025-11-26 13:49:19 -0500

    Firebase: Configutacion inicial en app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,3 +10,21 @@ import { RouterOutlet } from '@angular/router';
 export class App {
   protected readonly title = signal('Portafolio_Web_DJ');
 }
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAwclprMFEPkD6ILl-a3o89SM3gHbpcRds",
+  authDomain: "portafolioweb-b3b64.firebaseapp.com",
+  projectId: "portafolioweb-b3b64",
+  storageBucket: "portafolioweb-b3b64.firebasestorage.app",
+  messagingSenderId: "307826675398",
+  appId: "1:307826675398:web:a38879555618747bf8d1c7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
