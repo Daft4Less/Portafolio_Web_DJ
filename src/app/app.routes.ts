8b54ced61634a7cd62938f499bf66aea4b6bd9f2
Daft4Less <jose.jsu@gmail.com>
2025-11-25 19:39:15 -0500

    feat: generacion de inicio y ruta definida en la cabecera

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import path from 'path';
-import { Inicio } from './pages/inicio/inicio';
+import { Inicio } from './publico/inicio/inicio';
 
 export const routes: Routes = [
     {path: '', component: Inicio},
