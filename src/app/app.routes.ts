60db292c121c928007b8972d119bdc2b277abe1e
Daft4Less <jose.jsu@gmail.com>
2025-11-25 22:29:16 -0500

    Autenticacion: vista para el login el usuario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { Inicio } from './publico/inicio/inicio';
 import { Programadores } from './publico/programadores/programadores';
 import { VerPerfil } from './publico/ver-perfil/ver-perfil';
 import { Agendar } from './publico/agendar/agendar';
+import { Login } from './auth/login/login';
 
 export const routes: Routes = [
     {path: '', component: Inicio},
@@ -12,5 +13,6 @@ export const routes: Routes = [
     {path: 'programadores', component: Programadores},
     {path: 'empleado', component: VerPerfil},
     {path: 'agendar', component: Agendar},
+    {path: 'login', component: Login},
     {path: '**', redirectTo: ''}
 ];
